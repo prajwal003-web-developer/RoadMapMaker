6f5783246e1b05b96988a52526b54b02b3da7e00
prajwal003-web-developer <prajwalneupane003@gmail.com>
2026-01-07 20:53:34 +0545

    first commit

--- Backend/View/Project.View.js
+++ Backend/View/Project.View.js
@@ -10,7 +10,7 @@ const ProjectRouter = Router()
 ProjectRouter.post('/get-roadmap',requireAuth(),CreateProject)
 ProjectRouter.get('/mark-as-read/:topicId/:projectId',requireAuth(),MarkASRead)
 ProjectRouter.get('/mark-as-unread/:topicId/:projectId',requireAuth(),MarkASUnRead)
-ProjectRouter.get('/get-all',getRoadMap)
+ProjectRouter.get('/get-all',requireAuth(),getRoadMap)
 
 ProjectRouter.delete('/delete/:RoadmapId', requireAuth(),deleteRoadmap)
 
